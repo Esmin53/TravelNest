25463e8176e365fe69b0359736f859850e2ab145
Esmin53 <esmin.tufekcic53@gmail.com>
2024-01-07 08:06:21 +0100

    Homepage-featured: made property type carousel fully responsive

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -1,13 +1,12 @@
 "use client"
 
-import { Building, Dessert, GlassWater, Home, LucidePyramid, LucideSailboat, LucideSun, LucideTent, Mountain, Palmtree, Snowflake, TreePine } from "lucide-react"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel"
 import PropertyTypes from "./PropertyType"
 
 const Featured = () => {
     
     return (
-        <div className="w-full max-w-6xl gap-6 flex flex-col">
+        <div className="w-full max-w-6xl gap-6 flex flex-col p-2">
             <div className="w-full p-w flex flex-col gap-2">
                  <h1 className="text-xl font-bold">Trending Destinations</h1>
                  <p className="text-gray-500">Most popular choices for travelers worldwide</p>
@@ -32,109 +31,8 @@ const Featured = () => {
                     </div>
                  </div>
             </div>
-            <PropertyTypes />
+            <PropertyTypes /> 
 
-            <div className="w-full flex flex-col gap-4">
-                <h1 className="text-xl font-semibold">Find land that suits your style</h1>
-                <div className="flex gap-4">
-                    <div className="flex flex-col gap cursor-pointer items-center">
-                        <div className="w-14 h-14 rounded-sm flex flex-col items-center justify-center">
-                            <Building className="w-10 h-10" />
-                        </div>
-                        <p className="text-xs font-semibold">City</p>
-                    </div>
-                    <div className="flex flex-col gap cursor-pointer items-center">
-                        <div className="w-14 h-14 rounded-sm flex flex-col items-center justify-center">
-                            <Home className="w-10 h-10" />
-                        </div>
-                        <p className="text-xs font-semibold">Country</p>
-                    </div>
-                    <div className="flex flex-col gap cursor-pointer items-center">
-                        <div className="w-14 h-14 rounded-sm flex flex-col items-center justify-center">
-                            <Mountain className="w-10 h-10" />
-                        </div>
-                        <p className="text-xs font-semibold">Mountains</p>
-                    </div>
-                    <div className="flex flex-col gap cursor-pointer items-center">
-                        <div className="w-14 h-14 rounded-sm flex flex-col items-center justify-center">
-                            <LucideSun className="w-10 h-10" />
-                        </div>
-                        <p className="text-xs font-semibold">Beach</p>
-                    </div>
-                    <div className="flex flex-col gap cursor-pointer items-center">
-                        <div className="w-14 h-14 rounded-sm flex flex-col items-center justify-center">
-                            <TreePine className="w-10 h-10" />
-                        </div>
-                        <p className="text-xs font-semibold">Forrest</p>
-                    </div>
-                    <div className="flex flex-col gap cursor-pointer items-center">
-                        <div className="w-14 h-14 rounded-sm flex flex-col items-center justify-center">
-                            <LucidePyramid className="w-10 h-10" />
-                        </div>
-                        <p className="text-xs font-semibold">Desert</p>
-                    </div>
-                    <div className="flex flex-col gap cursor-pointer items-center">
-                        <div className="w-14 h-14 rounded-sm flex flex-col items-center justify-center">
-                            <Snowflake className="w-10 h-10" />
-                        </div>
-                        <p className="text-xs font-semibold">Snowy</p>
-                    </div>
-                    <div className="flex flex-col gap cursor-pointer items-center">
-                        <div className="w-14 h-14 rounded-sm flex flex-col items-center justify-center">
-                            <LucideTent className="w-10 h-10" />
-                        </div>
-                        <p className="text-xs font-semibold">Camping</p>
-                    </div>
-                    <div className="flex flex-col gap cursor-pointer items-center">
-                        <div className="w-14 h-14 rounded-sm flex flex-col items-center justify-center">
-                            <Palmtree className="w-10 h-10" />
-                        </div>
-                        <p className="text-xs font-semibold">Tropical</p>
-                    </div>
-                    <div className="flex flex-col gap cursor-pointer items-center">
-                        <div className="w-14 h-14 rounded-sm flex flex-col items-center justify-center">
-                            <LucideSailboat className="w-10 h-10" />
-                        </div>
-                        <p className="text-xs font-semibold">Island</p>
-                    </div>
-                </div>
-            </div>
-
-            <div className="w-full p-w flex flex-col gap-2">
-                 <h1 className="text-xl font-bold">Homes guests love</h1>
-                 <p className="text-gray-500">Most popular properties for our users</p>
-                 <Carousel>
-                    <CarouselContent className="-ml-1 h-72">
-                        <CarouselItem className="basis-1/3 p-1">
-                            <div className="w-full h-full rounded-md bg-orange-300">
-
-                            </div>
-                        </CarouselItem>
-                        <CarouselItem className="basis-1/3 p-1">
-                            <div className="w-full h-full rounded-md bg-orange-300">
-
-                            </div>
-                        </CarouselItem>
-                        <CarouselItem className="basis-1/3 p-1">
-                            <div className="w-full h-full rounded-md bg-orange-300">
-
-                            </div>
-                        </CarouselItem>
-                        <CarouselItem className="basis-1/3 p-1">
-                            <div className="w-full h-full rounded-md bg-orange-300">
-
-                            </div>
-                        </CarouselItem>
-                        <CarouselItem className="basis-1/4 p-1">
-                            <div className="w-full h-full rounded-md bg-orange-300">
-
-                            </div>
-                        </CarouselItem>
-                    </CarouselContent>
-                    <CarouselPrevious />
-                    <CarouselNext />
-                </Carousel>
-            </div>
         </div>
     )
 }
--- src/components/featured/PropertyType.tsx
+++ src/components/featured/PropertyType.tsx
@@ -1,23 +1,23 @@
 "use client"
 
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel"
-import {PropertyTypesArray} from "../../app/utils/data.js"
+import {PropertyTypesArray} from "../../utils/data.js"
 import Image from "next/image"
 
 const PropertyTypes = () => {
 
     return (
-        <div className="w-full p-w flex flex-col gap-2 py-2">
-        <h1 className="text-xl font-bold">Browse by property type</h1>
+        <div className="w-full p-w flex flex-col gap-1 sm:gap-2 py-2">
+        <h1 className="text-xl font-semibold sm:font-bold">Browse by property type</h1>
         {<Carousel>
-            <CarouselContent className="-ml-1 h-44">
+            <CarouselContent className="-ml-1 h-56 xs:h-44">
                 {PropertyTypesArray.map((item, index) => {
                     return (
-                        <CarouselItem className="basis-1/4 p-1 overflow-hidden" key={index}>
-                        <div className="w-full h-full rounded-md p-1 relative z-10 overflow-hidden">
-                            <Image src={item.image} fill alt={item.title} className="rounded-md z-20
-                            hover:scale-110 transition duration-500 cursor-pointer"/>
-                            <h1 className="text-lg text-white font-semibold 
+                        <CarouselItem className="xs:basis-1/2 md:basis-1/3 lg:basis-1/4 p-1 overflow-hidden" key={index}>
+                        <div className="w-full h-full xs:rounded-md p-1 relative z-10 ">
+                            <Image src={item.image} fill alt={item.title} className="rounded-sm xs:rounded-md z-20
+                            hover:scale-110 transition duration-500 cursor-pointer object-fill"/>
+                            <h1 className="text-xl xs:text-lg text-white font-semibold 
                             z-30 absolute top-1 left-2">{item.title}</h1>
                         </div>
                     </CarouselItem>
