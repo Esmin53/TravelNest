05252a47442f617e4f2d83a3194df3d31e775a81
Esmin53 <esmin.tufekcic53@gmail.com>
2024-01-28 23:51:01 +0100

    Add redirections to propertytype carousel

--- src/components/featured/PropertyType.tsx
+++ src/components/featured/PropertyType.tsx
@@ -3,8 +3,11 @@
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel"
 import {PropertyTypesArray} from "../../utils/data.js"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 
 const PropertyTypes = () => {
+
+    const router = useRouter()
 
     return (
         <div className="w-full p-w flex flex-col gap-1 sm:gap-2 py-2">
@@ -14,7 +17,8 @@ const PropertyTypes = () => {
                 {PropertyTypesArray.map((item, index) => {
                     return (
                         <CarouselItem className="xs:basis-1/2 md:basis-1/3 lg:basis-1/4 p-1 overflow-hidden xs:rounded-md" key={index}>
-                        <div className="w-full h-full xs:rounded-md p-1 relative z-10 ">
+                        <div className="w-full h-full xs:rounded-md p-1 relative z-10 " 
+                        onClick={() => router.push(`/accomodations?propertyType=${item.value}`)}>
                             <Image src={item.image} fill alt={item.title} className="rounded-sm xs:rounded-md z-20
                             hover:scale-105 transition duration-500 cursor-pointer object-fill"/>
                             <h1 className="text-xl xs:text-lg text-white font-semibold 
