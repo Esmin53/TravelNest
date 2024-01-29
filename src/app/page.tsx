f286b5702d7956d6c80a4a37639a1bb819297f31
Esmin53 <esmin.tufekcic53@gmail.com>
2024-01-29 01:45:24 +0100

    Finished favourite properties carouesl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
 import Navbar from "@/components/Navbar";
 import Search from "@/components/Search";
+import FavouriteProperties from "@/components/featured/FavouriteProperties";
 import Featured from "@/components/featured/Featured";
+import LandscapeType from "@/components/featured/LandscapeType";
+import { db } from "@/lib/db";
 
 
-export default function Home() {
+export default async function Home() {
+  
+  const favourite = await db.property.findMany({
+    orderBy: {
+      bookings: {
+        _count: 'desc'
+      }
+    },
+    include: {
+      bookings: true
+    },
+    take: 20
+  });
+  
   return (
     <main className="w-full flex flex-col items-center">
       <Navbar />
       <Search />
       <Featured />
+      <FavouriteProperties properties={favourite}/>
     </main>
   )
 }
---
+++ src/components/featured/FavouriteProperties.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { Property } from "@prisma/client";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel"
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const FavouriteProperties = ({properties}: {
+    properties: Property[]}) => {
+
+    const router = useRouter();
+
+    return (
+        <div className="max-w-7xl w-full p-w flex flex-col gap-2">
+                 <h1 className="text-xl font-bold">Accomodations guests love</h1>
+                 <p className="text-gray-500">Most popular accomodations by number of stays</p>
+                 <Carousel>
+                    <CarouselContent className="-ml-1 h-64 xs:h-72">
+                        {properties.map((item) => {
+                            return <CarouselItem className="sm:basis-1/2 lg:basis-1/3 p-1 cursor-pointer" key={item.id} 
+                            onClick={() => router.push(`/accomodations/${item.id}`)}>
+                            <div className="w-full h-full rounded-md overflow-hidden relative">
+                                <div className="w-full flex justify-between absolute bottom-2 px-2 z-30 text-lg text-white font-bold">
+                                    <p>{item.location}</p>
+                                    <p>{item.propertyType.charAt(0).toUpperCase() + item.propertyType.slice(1)}</p>
+                                </div>
+                                <h2 className="z-30 absolute bottom-8 left-2 text-3xl text-white font-semibold">{item.name}</h2>
+                                <Image src={item.images[0]} fill alt="Property image" className="z-10"/>
+                            </div>
+                        </CarouselItem>
+                        })}
+
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
+            </div>
+    )
+}
+
+export default FavouriteProperties;
