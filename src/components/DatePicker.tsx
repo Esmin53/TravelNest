c6ec20dd4ae4dd6325059a151c31b026440021f4
Esmin53 <esmin.tufekcic53@gmail.com>
2024-03-30 21:10:04 +0100

    Fix errors in book accomodation component

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -40,7 +40,7 @@ export function DatePicker({onChange, bookedDays, className}: {
   
     React.useEffect(() => {
         onChange(date)
-    }, [date, onChange])
+    }, [date])
 
   return (
     <Popover>
